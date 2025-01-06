5b4548405e6a0961c2d61e22dfd0d669e4ebf00e
Monnut <mast.monnut.boon@gmai.com>
2025-01-06 20:10:01 +0700

    End Of Section 8

--- client/src/app/layout/header/header.component.ts
+++ client/src/app/layout/header/header.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatBadgeModule} from '@angular/material/badge';
+import { MatIcon } from '@angular/material/icon';
+import { MatButtonModule} from '@angular/material/button';
+import { MatBadge} from '@angular/material/badge';
 
 
 @Component({
     selector: 'app-header',
     standalone: true,
-    imports: [MatIconModule, MatButtonModule, MatBadgeModule],
+    imports: [MatIcon, MatButtonModule, MatBadge],
     templateUrl: './header.component.html',
     styleUrl: './header.component.scss'
 })
